e60b5a15ff07bd5da703b6618d75202c25c15c19
Tania Boterman <taboterm@c2s4d9.42wolfsburg.de>
2023-03-27 18:08:50 +0200

    client added - wip

--- client.c
+++ client.c
@@ -6,10 +6,38 @@
 /*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 14:28:20 by taboterm          #+#    #+#             */
-/*   Updated: 2023/03/26 19:47:54 by taboterm         ###   ########.fr       */
+/*   Updated: 2023/03/27 18:02:43 by taboterm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <signal.h>
+#include <stdlib.h>
 
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	res;
+	int	sign;
+
+	i = 0;
+	res = 0;
+	sign = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] >= 48 && str[i] == 45)
+		i++;
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		res = res * 10 + str[i] - 48;
+		i++;
+	}
+	return (sign * res);
+}
+
+void	send_message(int pid, char *str)
+{
+	
+}
