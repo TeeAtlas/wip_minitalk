7b1b9667369b8ea073855913ada9b22b157db539
Tania Boterman <taboterm@c2s4d9.42wolfsburg.de>
2023-03-27 18:44:28 +0200

    client atoi changed

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: taboterm <taboterm@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 14:28:20 by taboterm          #+#    #+#             */
-/*   Updated: 2023/03/27 18:02:43 by taboterm         ###   ########.fr       */
+/*   Updated: 2023/03/27 18:42:36 by taboterm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ int	ft_atoi(const char *str)
 		i++;
 	if (str[i] == '-')
 		sign = -1;
-	if (str[i] >= 48 && str[i] == 45)
+	if (str[i] >= 48 || str[i] == 45)
 		i++;
-	while (str[i] >= 48 && str[i] <= 57)
+	while (str[i] >= 48 && str[i] <= 57) // could be handled with is digit function 
 	{
 		res = res * 10 + str[i] - 48;
 		i++;
@@ -39,5 +39,34 @@ int	ft_atoi(const char *str)
 
 void	send_message(int pid, char *str)
 {
+	int	i;
+	int	j;
 	
+	i = 0;
+	j = 0;
+	while (j < 7)
+	{
+		if ((str[i] & 1) == 0) // sending string as ascii to binary per character
+			kill(pid, SIGUSR1);
+		if ((str[i] & 1) == 1)
+			kill(pid, SIGUSR2);
+		str[i] = str[i] >> 1; // we are shifting bits to the right until statement is true
+		j++;
+		usleep(200); //using sleep to avoid a delay, wherein the information can get jumbled
+	}
+	i++;
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	
+	if (argc != 3)
+	{
+		write(1, "Invalid Args!\n ./client [PID] [STR]\n", 40);
+		exit(0);
+	}
+	pid = ft_atoi(argv[1]); //first argument which is the pid
+	send_message(pid, argv[2]); //second arguement is the string
+	return (0);
 }
